9bbf03dd9d26afb03b627c14aed42da837de8a61
Adam Hunter <ahunter@ccg.murdoch.edu.au>
2015-07-29 11:14:46 +0800

    don't set names vars unless set at runtime

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -18,8 +18,6 @@
 : ${CONFIG_mysql_dump_commcomp:='no'}
 : ${CONFIG_mysql_dump_latest:='no'}
 : ${CONFIG_mysql_dump_max_allowed_packet:=''}
-: ${CONFIG_db_names:=()}
-: ${CONFIG_db_month_names:=()}
 : ${CONFIG_db_exclude:=( 'information_schema' 'performance_schema' )}
 : ${CONFIG_mailcontent:='stdout'}
 : ${CONFIG_mail_maxattsize:=4000}
@@ -27,7 +25,15 @@
 : ${CONFIG_encrypt:='no'}
 : ${CONFIG_encrypt_password:='password0123'}
 
-export CONFIG_encrypt_password CONFIG_encrypt CONFIG_mail_address CONFIG_mail_maxattsize CONFIG_mailcontent CONFIG_db_exclude CONFIG_db_month_names CONFIG_db_names
+if [[ -v CONFIG_db_names ]]; then
+    export CONFIG_db_names
+fi
+
+if [[ -v CONFIG_month_names ]]; then
+    export CONFIG_month_names
+fi
+
+export CONFIG_encrypt_password CONFIG_encrypt CONFIG_mail_address CONFIG_mail_maxattsize CONFIG_mailcontent CONFIG_db_exclude
 export CONFIG_mysql_dump_max_allowed_packet CONFIG_mysql_dump_latest CONFIG_mysql_dump_commcomp CONFIG_mysql_dump_compression CONFIG_mysql_dump_use_separate_dirs CONFIG_mysql_dump_create_database
 export CONFIG_mysql_dump_socket CONFIG_mysql_dump_host CONFIG_mysql_dump_password CONFIG_mysql_dump_username CONFIG_mysql_dump_usessl CONFIG_rotation_monthly CONFIG_rotation_weekly
 export CONFIG_do_monthly CONFIG_backup_dir CONFIG_configfile
